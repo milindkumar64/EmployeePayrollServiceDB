99742368823f83c6acffde049eb1db78f0a6d785
milindkumar64 <milindgupta@gmail.com>
2023-05-04 10:49:23 +0530

    Add: Create table and show

--- src/addressbookdb.sql
+++ src/addressbookdb.sql
@@ -3,3 +3,8 @@ show databases;
 create database payroll_service;
 show databases;
 use payroll_service;
+
+-- UC2 --
+create table employee_payroll (id int not null auto_increment, name varchar(200) not null, 
+salary double not null, start_date date not null, primary key(id));
+
