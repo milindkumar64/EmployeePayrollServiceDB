4d823652d27191a446a453e36d57dde47bf564ab
milindkumar64 <milindgupta@gmail.com>
2023-05-04 11:20:00 +0530

    Add: insert into table and show

--- src/addressbookdb.sql
+++ src/addressbookdb.sql
@@ -1,5 +1,5 @@
 show databases;
- ---- UC1 -----
+ -- UC1 --
 create database payroll_service;
 show databases;
 use payroll_service;
@@ -8,3 +8,10 @@ use payroll_service;
 create table employee_payroll (id int not null auto_increment, name varchar(200) not null, 
 salary double not null, start_date date not null, primary key(id));
 
+-- UC3 --
+insert into employee_payroll(name,salary,start_date) values('Anish',10000.00,'2010-05-02');
+insert into employee_payroll(name,salary,start_date) values('Kalpana',10000.00,'2010-05-02');
+insert into employee_payroll(name,salary,start_date) values('Milind',2000.00,'2022-01-22');
+insert into employee_payroll(name,salary,start_date) values('Om',3000.00,'2020-11-09');
+
+select * from employee_payroll;
