56488e3949a29f77de0cc7099a1fda0d5a122f18
milindkumar64 <milindgupta@gmail.com>
2023-05-06 14:24:01 +0530

    Add: Insert Terissa as a new employee and set given data

--- src/addressbookdb.sql
+++ src/addressbookdb.sql
@@ -29,17 +29,18 @@ update employee_payroll set gender='F' where id=2;
 
 -- UC7 --
 
-select sum(salary) from employee_payroll where gender='F' group by gender;
-select count(name) from employee_payroll where gender='F' group by gender;
+select sum(salary) from employee_payroll where gender='F';
+select count(name) from employee_payroll where gender='M' group by gender;
 select min(salary) from employee_payroll;
 select max(salary) from employee_payroll;
 select avg(salary) from employee_payroll;
-
 
 ---- UC8 ----
 alter table employee_payroll add phone varchar(15) not null after start_date;
 alter table employee_payroll add address varchar(100) not null default 'Not Available' after phone;
 alter table employee_payroll add department varchar(100) not null after address;
+
+---- UC9 ----
 
 alter table employee_payroll add basic_pay double after department;
 alter table employee_payroll add deductions double after basic_pay;
@@ -47,8 +48,8 @@ alter table employee_payroll add taxable_pay double after deductions;
 alter table employee_payroll add income_pay double after taxable_pay;
 alter table employee_payroll add net_pay double after income_pay;
 
+update employee_payroll set gender='F' where name = 'Kalpana';
 select * from employee_payroll;
 
-
-
-
+---- UC10 ----
+insert into employee_payroll  values('6','Terissa','F','30000','2017-09-12','786578','Jaipur','HR','12000','1200','200','120','20000');
